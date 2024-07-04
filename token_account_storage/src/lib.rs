6c4db602b14c34f57a2ab933936f29f30dfde017
godmodegalactus <godmodegalactus@gmail.com>
2024-07-04 21:49:31 +0200

    adding token program and token 2022 program ids as constants

--- token_account_storage/src/lib.rs
+++ token_account_storage/src/lib.rs
@@ -1,3 +1,8 @@
+use solana_sdk::pubkey::Pubkey;
+
 pub mod account_types;
 pub mod inmemory_token_storage;
 pub mod token_program_utils;
+
+pub const TOKEN_PROGRAM_ID: Pubkey = spl_token::id();
+pub const TOKEN_PROGRAM_2022_ID: Pubkey = spl_token_2022::id();
