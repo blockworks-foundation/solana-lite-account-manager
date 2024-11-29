d4474088ec9ba27cdcf2f3b992417d94988f33c9
GroovieGermanikus <groovie@mango.markets>
2024-11-29 12:00:04 +0100

    rpc for chain data

--- simulate_from_snapshot/src/main.rs
+++ simulate_from_snapshot/src/main.rs
@@ -2,6 +2,7 @@ use std::{fs::File, path::PathBuf, str::FromStr, sync::Arc};
 
 use clap::Parser;
 use log::info;
+use mango_feeds_connector::chain_data::{ChainData, SlotData, SlotStatus};
 use cli::Args;
 use lite_account_manager_common::{
     account_data::{Account, AccountData, CompressionMethod, Data},
@@ -20,6 +21,10 @@ use quic_geyser_common::filters::AccountFilter;
 use quic_geyser_common::filters::Filter::AccountsAll;
 use snapshot_utils::{append_vec_iter, archived::ArchiveSnapshotExtractor, SnapshotExtractor};
 use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey};
+use solana_sdk::account::{AccountSharedData, ReadableAccount};
+use tokio::join;
+use lite_account_manager_common::account_filter::AccountFilterType;
+use lite_account_manager_common::account_store_interface::AccountLoadingError;
 use lite_account_manager_common::except_filter_store::ExceptFilterStore;
 use lite_account_manager_common::simple_filter_store::SimpleFilterStore;
 use lite_account_storage::inmemory_account_store::InmemoryAccountStore;
@@ -166,6 +171,7 @@ async fn main() {
     // // await for loading of snapshot to finish
     // bk.await.unwrap();
 
+    let mut chain_data = ChainData::new();
 
     let bk = {
         let storage = account_storage.clone();
@@ -179,6 +185,7 @@ async fn main() {
                 ArchiveSnapshotExtractor::open(&archive_path).unwrap();
             'snapshot_loop: for vec in loader.iter() {
                 let append_vec = vec.unwrap();
+                let slot = append_vec.slot();
                 // info!("size: {:?}", append_vec.len());
                 for handle in append_vec_iter(&append_vec) {
                     let stored = handle.access().unwrap();
@@ -186,7 +193,14 @@ async fn main() {
                     //     continue;
                     // }
 
+                    if "CbEq5Zo9Sn6krZvHPW2RaU9cR7EMMje9LwcNSQdhm6vG" == stored.meta.pubkey.to_string() {
+                        info!("loading account: {:?}", stored.meta.pubkey);
+                        info!(" -> size: {}", stored.meta.data_len);
+                    }
+
                     // info!("loading account: {:?}", stored.meta.pubkey);
+
+
                     let data = stored.data;
                     let compressed_data = Data::new(data, CompressionMethod::None);
                     storage.initilize_or_update_account(AccountData {
@@ -201,6 +215,31 @@ async fn main() {
                         updated_slot: 0,
                         write_version: 0,
                     });
+
+
+                    // chaindata copy
+
+                    chain_data.update_slot(SlotData {
+                        slot,
+                        parent: None,
+                        status: SlotStatus::Rooted, // =finalized
+                        chain: 0,
+                    });
+
+                    let account_shared_data = AccountSharedData::new(
+                        stored.account_meta.lamports,
+                        stored.stored_size,
+                        &stored.account_meta.owner);
+
+                    chain_data.update_account(
+                        stored.meta.pubkey,
+                        mango_feeds_connector::chain_data::AccountData {
+                            slot,
+                            write_version: 999999,
+                            account: account_shared_data,
+                        }
+                    );
+
                     limit_counter += 1;
                     // 7 secs
                     if limit_counter > 1_000_000 {
@@ -209,16 +248,86 @@ async fn main() {
                     }
                 }
             }
+
+            return chain_data;
         })
     };
     // await for loading of snapshot to finish
-    bk.await.unwrap();
-
+    let chain_data = bk.await.unwrap();
+    let chain_data_arc = Arc::new(chain_data);
 
     log::info!("Storage Initialized with snapshot");
-    let rpc_server = RpcServerImpl::new(account_storage);
-    let jh = RpcServerImpl::start_serving(rpc_server, 10700)
-        .await
-        .unwrap();
-    let _ = jh.await;
+
+    let jh_rpc_on_lite = {
+        info!("Starting RPC with account storage lite");
+        let rpc_server = RpcServerImpl::new(account_storage);
+        let jh_rpc_on_lite = RpcServerImpl::start_serving(rpc_server, 10700)
+            .await
+            .unwrap();
+
+        jh_rpc_on_lite
+    };
+
+
+    let jh_rpc_on_chaindata = {
+        info!("Starting RPC with chain data");
+        let chain_data_wrapper = Arc::new(ChainDataWrapper(chain_data_arc));
+        let rpc_server = RpcServerImpl::new(chain_data_wrapper);
+        let jh_rpc_on_chaindata = RpcServerImpl::start_serving(rpc_server, 10701)
+            .await
+            .unwrap();
+
+        jh_rpc_on_chaindata
+    };
+
+    let _results = join!(jh_rpc_on_lite, jh_rpc_on_chaindata);
+
+
+}
+
+struct ChainDataWrapper(Arc<ChainData>);
+
+impl AccountStorageInterface for ChainDataWrapper {
+    fn update_account(&self, account_data: AccountData, commitment: Commitment) -> bool {
+        todo!()
+    }
+
+    fn initilize_or_update_account(&self, account_data: AccountData) {
+        todo!()
+    }
+
+    fn get_account(&self, account_pk: Pubkey, commitment: Commitment) -> Result<Option<AccountData>, AccountLoadingError> {
+        let account = self.0.account(&account_pk).map_err(|e| AccountLoadingError::AccountNotFound)?;
+
+        let rpc = AccountData {
+            pubkey: account_pk,
+            account: Arc::new(Account {
+                lamports: account.account.lamports(),
+                data: Data::new(account.account.data(), CompressionMethod::None),
+                owner: account.account.owner().clone(),
+                executable: false, // TODO
+                rent_epoch: 0, // RTODO
+            }),
+            updated_slot: 0, // TODO
+            write_version: 0, //TODO
+        };
+
+        Ok(Some(rpc))
+    }
+
+    fn get_program_accounts(&self, program_pubkey: Pubkey, account_filter: Option<Vec<AccountFilterType>>, commitment: Commitment) -> Result<Vec<AccountData>, AccountLoadingError> {
+        todo!()
+    }
+
+    fn process_slot_data(&self, slot_info: SlotInfo, commitment: Commitment) -> Vec<AccountData> {
+        todo!()
+    }
+
+    fn create_snapshot(&self, program_id: Pubkey) -> Result<Vec<u8>, AccountLoadingError> {
+        todo!()
+    }
+
+    fn load_from_snapshot(&self, program_id: Pubkey, snapshot: Vec<u8>) -> Result<(), AccountLoadingError> {
+        todo!()
+    }
 }
