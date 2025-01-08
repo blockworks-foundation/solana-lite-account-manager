c30b1e39a77d4e22ab6ec01a599c7f4095262a75
GroovieGermanikus <groovie@mango.markets>
2025-01-08 14:56:55 +0100

    configure logger

--- accounts_from_snapshot/examples/download_incremental_snapshot.rs
+++ accounts_from_snapshot/examples/download_incremental_snapshot.rs
@@ -8,17 +8,19 @@ use solana_sdk::epoch_schedule::Slot;
 
 use lite_accounts_from_snapshot::{Config, HostUrl, Loader};
 
-pub struct TestConsumer {}
-
 #[tokio::main]
 async fn main() {
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
+
     let temp_dir = env::temp_dir();
     let full_snapshot_path = temp_dir.join("full-snapshot");
     let incremental_snapshot_path = temp_dir.join("incremental-snapshot-incr");
 
     let loader = Loader::new(Config {
         hosts: Box::new([HostUrl::from_str("https://api.testnet.solana.com").unwrap()]),
-        not_before_slot: Slot::from(0u64),
+        not_before_slot: Slot::from(312666355u64),
         full_snapshot_path,
         incremental_snapshot_path,
         maximum_full_snapshot_archives_to_retain: NonZeroUsize::new(100).unwrap(),
