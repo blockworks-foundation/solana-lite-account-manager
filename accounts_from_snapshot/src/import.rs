04df171c05d4a750cc28e1b2f6011776195542b2
GroovieGermanikus <groovie@mango.markets>
2025-01-09 00:38:25 +0100

    cleanup

--- accounts_from_snapshot/src/import.rs
+++ accounts_from_snapshot/src/import.rs
@@ -34,34 +34,33 @@ pub async fn import_archive(archive_path: PathBuf) -> (Receiver<AccountData>, Jo
         for append_vec in extractor.iter() {
             let tx = tx.clone();
 
-            // tokio::task::spawn(async move {
-                let append_vec = append_vec.unwrap();
+            let append_vec = append_vec.unwrap();
 
-            // TODO use rayon
-                for handle in append_vec_iter(&append_vec) {
-                    if let Some((account_meta, _offset)) = append_vec.get_account(handle.offset) {
-                        cnt_append_vecs += 1;
-                        if cnt_append_vecs % 100_000 == 0 {
-                            info!("{} append vecs loaded after {:.3}s (speed {:.0}/s)",
-                            cnt_append_vecs, started_at.elapsed().as_secs_f64(), cnt_append_vecs as f64 / started_at.elapsed().as_secs_f64());
-                        }
+            for handle in append_vec_iter(&append_vec) {
+                if let Some((account_meta, _offset)) = append_vec.get_account(handle.offset) {
+                    cnt_append_vecs += 1;
+                    if cnt_append_vecs % 100_000 == 0 {
+                        info!("{} append vecs loaded after {:.3}s (speed {:.0}/s)",
+                        cnt_append_vecs, started_at.elapsed().as_secs_f64(), cnt_append_vecs as f64 / started_at.elapsed().as_secs_f64());
 
-                        tx.blocking_send(AccountData {
-                            pubkey: account_meta.meta.pubkey,
-                            account: Arc::new(Account {
-                                lamports: account_meta.account_meta.lamports,
-                                data: Data::Uncompressed(Vec::from(account_meta.data)),
-                                owner: account_meta.account_meta.owner.clone(),
-                                executable: account_meta.account_meta.executable,
-                                rent_epoch: account_meta.account_meta.rent_epoch,
-                            }),
-                            updated_slot: append_vec.slot(),
-                            write_version: 0,
-                        })
-                        .expect("Failed to send account data");
+                        info!("items in channel: {}", tx.max_capacity() - tx.capacity());
                     }
+
+                    tx.blocking_send(AccountData {
+                        pubkey: account_meta.meta.pubkey,
+                        account: Arc::new(Account {
+                            lamports: account_meta.account_meta.lamports,
+                            data: Data::Uncompressed(Vec::from(account_meta.data)),
+                            owner: account_meta.account_meta.owner.clone(),
+                            executable: account_meta.account_meta.executable,
+                            rent_epoch: account_meta.account_meta.rent_epoch,
+                        }),
+                        updated_slot: append_vec.slot(),
+                        write_version: 0,
+                    })
+                    .expect("Failed to send account data");
                 }
-            // });
+            }
         }
     });
 
