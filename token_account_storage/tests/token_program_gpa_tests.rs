3ca60247e5a888cb026f96cd2768ffee14b8c93a
armedx86 <90354404+armedx86@users.noreply.github.com>
2025-01-20 17:14:09 +0100

    Use imports instead of fully qualifying symbols

--- token_account_storage/tests/token_program_gpa_tests.rs
+++ token_account_storage/tests/token_program_gpa_tests.rs
@@ -8,9 +8,12 @@ use solana_sdk::pubkey::Pubkey;
 mod utils;
 
 use lite_account_manager_common::{
-    account_filter::{AccountFilterType, MemcmpFilter},
+    account_filter::{AccountFilterType, MemcmpFilter, MemcmpFilterData},
     account_store_interface::AccountStorageInterface,
     commitment::Commitment,
+};
+use utils::{
+    create_mint_account_data, create_token_account_data, MintCreationParams, TokenAccountParams,
 };
 
 #[test]
@@ -19,12 +22,12 @@ pub fn test_gpa_token_account() {
     let token_store = TokenProgramAccountsStorage::new(inmemory_token_storage);
 
     let mint_1: Pubkey = Pubkey::new_unique();
-    let mint_creation_params = utils::MintCreationParams::create_default(100);
-    let mint_account_1 = utils::create_mint_account_data(mint_1, mint_creation_params, 0, 0);
+    let mint_creation_params = MintCreationParams::create_default(100);
+    let mint_account_1 = create_mint_account_data(mint_1, mint_creation_params, 0, 0);
 
     let mint_2: Pubkey = Pubkey::new_unique();
-    let mint_creation_params = utils::MintCreationParams::create_default(100);
-    let mint_account_2 = utils::create_mint_account_data(mint_2, mint_creation_params, 0, 0);
+    let mint_creation_params = MintCreationParams::create_default(100);
+    let mint_account_2 = create_mint_account_data(mint_2, mint_creation_params, 0, 0);
 
     let owner_1 = Pubkey::new_unique();
     let owner_2 = Pubkey::new_unique();
@@ -33,27 +36,27 @@ pub fn test_gpa_token_account() {
     let token_acc_1_1 = Pubkey::new_unique();
     let token_acc_1_2 = Pubkey::new_unique();
     let token_acc_1_3 = Pubkey::new_unique();
-    let token_acc_1_1_params = utils::TokenAccountParams::create_default(owner_1, mint_1, 100);
-    let token_acc_1_2_params = utils::TokenAccountParams::create_default(owner_1, mint_2, 200);
-    let token_acc_1_3_params = utils::TokenAccountParams::create_default(owner_1, mint_2, 300);
+    let token_acc_1_1_params = TokenAccountParams::create_default(owner_1, mint_1, 100);
+    let token_acc_1_2_params = TokenAccountParams::create_default(owner_1, mint_2, 200);
+    let token_acc_1_3_params = TokenAccountParams::create_default(owner_1, mint_2, 300);
     let token_acc_1_1_account =
-        utils::create_token_account_data(token_acc_1_1, token_acc_1_1_params, 0, 0);
+        create_token_account_data(token_acc_1_1, token_acc_1_1_params, 0, 0);
     let token_acc_1_2_account =
-        utils::create_token_account_data(token_acc_1_2, token_acc_1_2_params, 0, 0);
+        create_token_account_data(token_acc_1_2, token_acc_1_2_params, 0, 0);
     let token_acc_1_3_account =
-        utils::create_token_account_data(token_acc_1_3, token_acc_1_3_params, 0, 0);
+        create_token_account_data(token_acc_1_3, token_acc_1_3_params, 0, 0);
     token_store.initialize_or_update_account(token_acc_1_1_account.clone());
     token_store.initialize_or_update_account(token_acc_1_2_account.clone());
     token_store.initialize_or_update_account(token_acc_1_3_account.clone());
 
     let token_acc_2_1 = Pubkey::new_unique();
     let token_acc_2_2 = Pubkey::new_unique();
-    let token_acc_2_1_params = utils::TokenAccountParams::create_default(owner_2, mint_1, 400);
-    let token_acc_2_2_params = utils::TokenAccountParams::create_default(owner_2, mint_1, 500);
+    let token_acc_2_1_params = TokenAccountParams::create_default(owner_2, mint_1, 400);
+    let token_acc_2_2_params = TokenAccountParams::create_default(owner_2, mint_1, 500);
     let token_acc_2_1_account =
-        utils::create_token_account_data(token_acc_2_1, token_acc_2_1_params, 0, 0);
+        create_token_account_data(token_acc_2_1, token_acc_2_1_params, 0, 0);
     let token_acc_2_2_account =
-        utils::create_token_account_data(token_acc_2_2, token_acc_2_2_params, 0, 0);
+        create_token_account_data(token_acc_2_2, token_acc_2_2_params, 0, 0);
     token_store.initialize_or_update_account(token_acc_2_1_account.clone());
     token_store.initialize_or_update_account(token_acc_2_2_account.clone());
 
@@ -62,21 +65,21 @@ pub fn test_gpa_token_account() {
     let token_acc_3_3 = Pubkey::new_unique();
     let token_acc_3_4 = Pubkey::new_unique();
     let token_acc_3_5 = Pubkey::new_unique();
-    let token_acc_3_1_params = utils::TokenAccountParams::create_default(owner_3, mint_1, 600);
-    let token_acc_3_2_params = utils::TokenAccountParams::create_default(owner_3, mint_1, 700);
-    let token_acc_3_3_params = utils::TokenAccountParams::create_default(owner_3, mint_2, 800);
-    let token_acc_3_4_params = utils::TokenAccountParams::create_default(owner_3, mint_2, 900);
-    let token_acc_3_5_params = utils::TokenAccountParams::create_default(owner_3, mint_1, 1000);
+    let token_acc_3_1_params = TokenAccountParams::create_default(owner_3, mint_1, 600);
+    let token_acc_3_2_params = TokenAccountParams::create_default(owner_3, mint_1, 700);
+    let token_acc_3_3_params = TokenAccountParams::create_default(owner_3, mint_2, 800);
+    let token_acc_3_4_params = TokenAccountParams::create_default(owner_3, mint_2, 900);
+    let token_acc_3_5_params = TokenAccountParams::create_default(owner_3, mint_1, 1000);
     let token_acc_3_1_account =
-        utils::create_token_account_data(token_acc_3_1, token_acc_3_1_params, 0, 0);
+        create_token_account_data(token_acc_3_1, token_acc_3_1_params, 0, 0);
     let token_acc_3_2_account =
-        utils::create_token_account_data(token_acc_3_2, token_acc_3_2_params, 0, 0);
+        create_token_account_data(token_acc_3_2, token_acc_3_2_params, 0, 0);
     let token_acc_3_3_account =
-        utils::create_token_account_data(token_acc_3_3, token_acc_3_3_params, 0, 0);
+        create_token_account_data(token_acc_3_3, token_acc_3_3_params, 0, 0);
     let token_acc_3_4_account =
-        utils::create_token_account_data(token_acc_3_4, token_acc_3_4_params, 0, 0);
+        create_token_account_data(token_acc_3_4, token_acc_3_4_params, 0, 0);
     let token_acc_3_5_account =
-        utils::create_token_account_data(token_acc_3_5, token_acc_3_5_params, 0, 0);
+        create_token_account_data(token_acc_3_5, token_acc_3_5_params, 0, 0);
     token_store.initialize_or_update_account(token_acc_3_1_account.clone());
     token_store.initialize_or_update_account(token_acc_3_2_account.clone());
     token_store.initialize_or_update_account(token_acc_3_3_account.clone());
@@ -93,9 +96,7 @@ pub fn test_gpa_token_account() {
                 AccountFilterType::DataSize(165),
                 AccountFilterType::Memcmp(MemcmpFilter {
                     offset: 32,
-                    data: lite_account_manager_common::account_filter::MemcmpFilterData::Bytes(
-                        owner_1.to_bytes().to_vec(),
-                    ),
+                    data: MemcmpFilterData::Bytes(owner_1.to_bytes().to_vec()),
                 }),
             ]),
             Commitment::Processed,
@@ -113,9 +114,7 @@ pub fn test_gpa_token_account() {
                 AccountFilterType::DataSize(165),
                 AccountFilterType::Memcmp(MemcmpFilter {
                     offset: 32,
-                    data: lite_account_manager_common::account_filter::MemcmpFilterData::Bytes(
-                        owner_2.to_bytes().to_vec(),
-                    ),
+                    data: MemcmpFilterData::Bytes(owner_2.to_bytes().to_vec()),
                 }),
             ]),
             Commitment::Processed,
@@ -132,9 +131,7 @@ pub fn test_gpa_token_account() {
                 AccountFilterType::DataSize(165),
                 AccountFilterType::Memcmp(MemcmpFilter {
                     offset: 32,
-                    data: lite_account_manager_common::account_filter::MemcmpFilterData::Bytes(
-                        owner_3.to_bytes().to_vec(),
-                    ),
+                    data: MemcmpFilterData::Bytes(owner_3.to_bytes().to_vec()),
                 }),
             ]),
             Commitment::Processed,
@@ -154,15 +151,11 @@ pub fn test_gpa_token_account() {
                 AccountFilterType::DataSize(165),
                 AccountFilterType::Memcmp(MemcmpFilter {
                     offset: 32,
-                    data: lite_account_manager_common::account_filter::MemcmpFilterData::Bytes(
-                        owner_3.to_bytes().to_vec(),
-                    ),
+                    data: MemcmpFilterData::Bytes(owner_3.to_bytes().to_vec()),
                 }),
                 AccountFilterType::Memcmp(MemcmpFilter {
                     offset: 0,
-                    data: lite_account_manager_common::account_filter::MemcmpFilterData::Bytes(
-                        mint_1.to_bytes().to_vec(),
-                    ),
+                    data: MemcmpFilterData::Bytes(mint_1.to_bytes().to_vec()),
                 }),
             ]),
             Commitment::Processed,
@@ -181,9 +174,7 @@ pub fn test_gpa_token_account() {
                 AccountFilterType::DataSize(165),
                 AccountFilterType::Memcmp(MemcmpFilter {
                     offset: 0,
-                    data: lite_account_manager_common::account_filter::MemcmpFilterData::Bytes(
-                        mint_1.to_bytes().to_vec(),
-                    ),
+                    data: MemcmpFilterData::Bytes(mint_1.to_bytes().to_vec()),
                 }),
             ]),
             Commitment::Confirmed,
@@ -204,9 +195,7 @@ pub fn test_gpa_token_account() {
                 AccountFilterType::DataSize(165),
                 AccountFilterType::Memcmp(MemcmpFilter {
                     offset: 0,
-                    data: lite_account_manager_common::account_filter::MemcmpFilterData::Bytes(
-                        mint_2.to_bytes().to_vec(),
-                    ),
+                    data: MemcmpFilterData::Bytes(mint_2.to_bytes().to_vec()),
                 }),
             ]),
             Commitment::Confirmed,
