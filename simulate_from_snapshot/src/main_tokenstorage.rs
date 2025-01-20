878c8e680cbd7168b6251528252f0e51d34bbfc2
GroovieGermanikus <groovie@mango.markets>
2025-01-20 17:27:01 +0100

    WIP download snapshot

--- simulate_from_snapshot/src/main_tokenstorage.rs
+++ simulate_from_snapshot/src/main_tokenstorage.rs
@@ -1,9 +1,12 @@
 use std::collections::HashMap;
+use std::num::NonZeroUsize;
+use std::sync::OnceLock;
 use std::time::Duration;
 use std::{env, path::PathBuf, str::FromStr, sync::Arc};
 
 use clap::Parser;
 use cli::Args;
+use futures::stream::Once;
 use geyser_grpc_connector::grpc_subscription_autoreconnect_tasks::create_geyser_autoconnection_task_with_mpsc;
 use geyser_grpc_connector::{GrpcConnectionTimeouts, GrpcSourceConfig};
 use lite_account_manager_common::{
@@ -12,7 +15,9 @@ use lite_account_manager_common::{
     commitment::Commitment,
     slot_info::SlotInfo,
 };
+use lite_account_storage::accountsdb::AccountsDb;
 use lite_accounts_from_snapshot::import::import_archive;
+use lite_accounts_from_snapshot::{start_import_from_snapshot, Config, HostUrl};
 use lite_token_account_storage::{
     inmemory_token_account_storage::InmemoryTokenAccountStorage,
     inmemory_token_storage::TokenProgramAccountsStorage,
@@ -22,6 +27,8 @@ use quic_geyser_common::{filters::Filter, types::connections_parameters::Connect
 use solana_sdk::clock::Slot;
 use solana_sdk::commitment_config::CommitmentConfig;
 use solana_sdk::pubkey::Pubkey;
+use tokio::sync::mpsc::Receiver;
+use tokio::sync::OnceCell;
 use tokio::task::JoinHandle;
 use yellowstone_grpc_proto::geyser::subscribe_update::UpdateOneof;
 use yellowstone_grpc_proto::geyser::{
@@ -55,74 +62,84 @@ async fn main() {
         TokenProgramAccountsStorage::new(token_account_storage.clone()),
     );
 
+    let (_jh, rx_aos) = match (quic_url, grpc_addr) {
+        (Some(quic_url), None) => {
+            info!("Using quic source on {}", quic_url);
+            let (jh, rx_aos) =
+                stream_accounts_from_quic_geyser_plugin(quic_url, token_storage.clone());
+            (jh, Some(rx_aos))
+        }
+        (Some(quic_url), Some(grpc_addr)) => {
+            panic!("Only one source (grpc or quic plugin) can be specified");
+        }
+        (None, Some(grpc_addr)) => {
+            info!(
+                "Using grpc source on {} (token: {})",
+                grpc_addr,
+                grpc_x_token.is_some()
+            );
+            let (jh, rx_aos) = stream_accounts_from_yellowstone_grpc(
+                grpc_addr,
+                grpc_x_token,
+                token_storage.clone(),
+            );
+            (jh, Some(rx_aos))
+        }
+        (None, None) => {
+            // TODO handle this case
+            panic!("No source specified");
+        }
+    };
+
     // load accounts from snapshot
     let token_storage = token_storage.clone();
-    let archive_path = PathBuf::from_str(snapshot_archive_path.as_str()).unwrap();
 
-    log::info!("Start importing accounts from full snapshot");
-    let (mut accounts_rx, _) = import_archive(archive_path).await;
-    let mut num_token_accounts = 0u64;
-    let mut num_mint_accounts = 0u64;
-    while let Some(AccountData {
-        account, pubkey, ..
-    }) = accounts_rx.recv().await
-    {
-        if account.owner != spl_token::ID && account.owner != spl_token_2022::ID {
-            continue;
+    if let Some(mut rx_aos) = rx_aos {
+        log::info!("Start importing accounts from snapshot");
+        let mut num_token_accounts = 0u64;
+        let mut num_mint_accounts = 0u64;
+        while let Some(aos) = rx_aos.recv().await
+        {
+            match aos {
+                AccountOrSlotFromGeyser::Account(account_data) => {
+                    let account = &account_data.account;
+                    if account.owner != spl_token::ID && account.owner != spl_token_2022::ID {
+                        continue;
+                    }
+
+                    // load only mints from snapshot
+                    // if account.data.len() != 82 {
+                    //     continue;
+                    // }
+
+                    if account.data.len() != 82 {
+                        num_token_accounts += 1;
+                        continue;
+                    } else {
+                        num_mint_accounts += 1;
+                    }
+
+                    let data = account.data.clone();
+                    token_storage.initialize_or_update_account(account_data);
+                    if (num_token_accounts + num_mint_accounts) % 100_000 == 0 {
+                        log::info!(
+                        "{} spl token and {} mint accounts loaded",
+                        num_token_accounts,
+                        num_mint_accounts
+                    );
+                    }
+                }
+                AccountOrSlotFromGeyser::Slot(_, _) => {}
+            }
         }
 
-        // load only mints from snapshot
-        // if account.data.len() != 82 {
-        //     continue;
-        // }
-
-        if account.data.len() != 82 {
-            num_token_accounts += 1;
-            continue;
-        } else {
-            num_mint_accounts += 1;
-        }
-
-        let data = account.data.clone();
-        token_storage.initialize_or_update_account(AccountData {
-            pubkey,
-            account: Arc::new(Account {
-                lamports: account.lamports,
-                data,
-                owner: account.owner,
-                executable: account.executable,
-                rent_epoch: account.rent_epoch,
-            }),
-            updated_slot: 0,
-            write_version: 0,
-        });
-        if (num_token_accounts + num_mint_accounts) % 100_000 == 0 {
-            log::info!(
-                "{} spl token and {} mint accounts loaded",
-                num_token_accounts,
-                num_mint_accounts
-            );
-        }
-    }
-
-    log::info!(
-        "Storage Initialized with snapshot, {} spl program accounts loaded",
-        num_token_accounts + num_mint_accounts
-    );
-
-    if let Some(quic_url) = quic_url {
-        info!("Using quic source on {}", quic_url);
-        stream_accounts_from_quic_geyser_plugin(quic_url, token_storage.clone());
-    }
-
-    if let Some(grpc_addr) = grpc_addr {
-        info!(
-            "Using grpc source on {} (token: {})",
-            grpc_addr,
-            grpc_x_token.is_some()
+        log::info!(
+            "Storage Initialized with snapshot, {} spl program accounts loaded",
+            num_token_accounts + num_mint_accounts
         );
-        let _jh =
-            stream_accounts_from_yellowstone_grpc(grpc_addr, grpc_x_token, token_storage.clone());
+    } else {
+        info!("Just load from snapshot but do not stream from geyser");
+        todo!()
     }
 
     let rpc_server = RpcServerImpl::new(token_storage.clone(), Some(token_storage));
@@ -131,14 +148,51 @@ async fn main() {
         .unwrap();
 }
 
+fn build_snapshot_config(not_before_slot: Slot) -> Config {
+    let temp_dir = env::temp_dir();
+    let full_snapshot_path = temp_dir.join("full-snapshot");
+    let incremental_snapshot_path = temp_dir.join("incremental-snapshot");
+
+    Config {
+        hosts: vec![
+            // testnet
+            // testnet validator in /home/groovie on fcs-ams1
+            // HostUrl::from_str("http://178.237.58.250:8899").unwrap(),
+            // HostUrl::from_str("http://147.28.178.75:8899").unwrap(),
+            // HostUrl::from_str("http://204.13.239.110:8899").unwrap(),
+            // HostUrl::from_str("http://149.50.110.119:8899").unwrap(),
+            // HostUrl::from_str("http://146.59.54.19:8899").unwrap(),
+            // HostUrl::from_str("http://74.50.77.158:80").unwrap(),
+            // HostUrl::from_str("http://149.50.104.41:8899").unwrap(),
+            // HostUrl::from_str("http://205.209.109.158:8899").unwrap(),
+            // mainnet
+            HostUrl::from_str("http://80.77.175.84:8899").unwrap(),
+            HostUrl::from_str("http://108.61.89.6:8899").unwrap(),
+            HostUrl::from_str("http://147.28.173.107:8899").unwrap(),
+        ]
+        .into_boxed_slice(),
+        not_before_slot,
+        full_snapshot_path,
+        incremental_snapshot_path,
+        maximum_full_snapshot_archives_to_retain: NonZeroUsize::new(10).unwrap(),
+        maximum_incremental_snapshot_archives_to_retain: NonZeroUsize::new(10).unwrap(),
+    }
+}
+
+enum AccountOrSlotFromGeyser {
+    // you can assume processed
+    Account(AccountData),
+    Slot(SlotInfo, Commitment),
+}
+
 fn stream_accounts_from_quic_geyser_plugin(
     quic_url: String,
-    token_storage: Arc<TokenProgramAccountsStorage>,
-) {
+) -> (JoinHandle<()>, Receiver<AccountOrSlotFromGeyser>) {
     use quic_geyser_common::message::Message;
 
-    let token_storage = token_storage.clone();
-    tokio::spawn(async move {
+    let (tx, rx) = tokio::sync::mpsc::channel(1024);
+
+    let jh = tokio::spawn(async move {
         let (quic_client, mut receiver, _jh) =
             quic_geyser_client::non_blocking::client::Client::new(
                 quic_url,
@@ -176,10 +230,9 @@ fn stream_accounts_from_quic_geyser_plugin(
                         updated_slot: account.slot_identifier.slot,
                         write_version: account.write_version,
                     };
-                    token_storage.update_account(
-                        account_data,
-                        lite_account_manager_common::commitment::Commitment::Processed,
-                    );
+                    tx.send(AccountOrSlotFromGeyser::Account(account_data))
+                        .await
+                        .unwrap(); // TODO improve
                 }
                 Message::SlotMsg(slot_msg) => {
                     if slot_msg.commitment_config == CommitmentConfig::confirmed()
@@ -191,14 +244,11 @@ fn stream_accounts_from_quic_geyser_plugin(
                             } else {
                                 Commitment::Finalized
                             };
-                        token_storage.process_slot_data(
-                            SlotInfo {
-                                slot: slot_msg.slot,
-                                parent: slot_msg.parent,
-                                root: 0,
-                            },
-                            commitment,
-                        );
+                        tx.send(AccountOrSlotFromGeyser::Slot(SlotInfo {
+                            slot: slot_msg.slot,
+                            parent: slot_msg.parent,
+                            root: 0,
+                        }, commitment)).await.unwrap();
                     }
                 }
                 _ => {
@@ -210,16 +260,21 @@ fn stream_accounts_from_quic_geyser_plugin(
         println!("stopping geyser stream");
         log::error!("stopping geyser stream");
     });
+
+    (jh, rx)
 }
 
 fn stream_accounts_from_yellowstone_grpc(
     grpc_addr: String,
     grpc_x_token: Option<String>,
     token_storage: Arc<TokenProgramAccountsStorage>,
-) -> JoinHandle<()> {
+) -> (JoinHandle<()>, Receiver<AccountOrSlotFromGeyser>) {
     use geyser_grpc_connector::Message;
 
-    tokio::spawn(async move {
+    let (tx, rx) = tokio::sync::mpsc::channel(1024);
+    // let (tx_first_slot, rx_first_slot) = tokio::sync::oneshot::channel();
+
+    let jh = tokio::spawn(async move {
         info!(
             "Using grpc source on {} ({})",
             grpc_addr,
@@ -252,6 +307,7 @@ fn stream_accounts_from_yellowstone_grpc(
                     Some(UpdateOneof::Account(update_account)) => {
                         let account_info = update_account.account.unwrap();
                         let slot = update_account.slot;
+                        // let _once = tx_first_slot.set(slot + 1);
                         let pubkey =
                             Pubkey::new_from_array(account_info.pubkey.try_into().unwrap());
                         let lamports = account_info.lamports;
@@ -313,7 +369,9 @@ fn stream_accounts_from_yellowstone_grpc(
         } // -- recv loop
 
         log::error!("stopping geyser stream");
-    })
+    });
+
+    (jh, rx)
 }
 
 /// 1. slots with all commitment levels
@@ -356,4 +414,9 @@ fn map_slot_status(
             yCL::Finalized => solanaCL::Finalized,
         })
         .expect("valid commitment level")
+}
+
+fn start_backfill(not_before_slot: Slot, db: Arc<AccountsDb>) {
+    let config = build_snapshot_config(not_before_slot);
+    let _jh = start_backfill_import_from_snapshot(config, db);
 }
